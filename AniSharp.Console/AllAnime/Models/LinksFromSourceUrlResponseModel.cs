96bd1d6e720af699800d5525252272c2e3884952
Minko0 <minkoarc@gmail.com>
2025-01-15 14:12:52 +0100

    Fix repackager links

--- AniSharp.Console/AllAnime/Models/LinksFromSourceUrlResponseModel.cs
+++ AniSharp.Console/AllAnime/Models/LinksFromSourceUrlResponseModel.cs
@@ -4,7 +4,8 @@ public class LinksFromSourceUrlResponseModel
 {
     public class LinkType
     {
-        public string Link { get; set; }
+        public string? Link { get; set; }
+        public string ResolutionStr { get; set; }
     }
 
     public IEnumerable<LinkType> Links { get; set; }
--- AniSharp.Console/Program.cs
+++ AniSharp.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 using AniSharp.Console.AllAnime;
 using Spectre.Console;
 using static AniSharp.Console.AllAnime.SearchAnimeResponseModel.DataType;
@@ -22,20 +23,28 @@ while (results.Count == 0)
     results = result.Data.Shows.Edges.ToList();
 }
 
-
 AnsiConsole.Clear();
 var selectShowPrompt = new SelectionPrompt<ShowsType.EdgesType>()
     .Title("Select show:")
-    .PageSize(results.Count)
     .AddChoices(results);
+
+if (results.Count >= 3)
+{
+    selectShowPrompt.PageSize(results.Count);
+}
+
 selectShowPrompt.Converter = type => $"{type.Name} ({type.AvailableEpisodes.Sub} episodes)";
 
 var episode = AnsiConsole.Prompt(selectShowPrompt);
 
 AnsiConsole.Clear();
 var selectEpisodePrompt = new SelectionPrompt<int>()
-    .Title("Select episode:")
-    .PageSize(episode.AvailableEpisodes.Sub);
+    .Title("Select episode:");
+
+if (episode.AvailableEpisodes.Sub >= 3)
+{
+    selectShowPrompt.PageSize(episode.AvailableEpisodes.Sub);
+}
 
 for (var i = 1; i <= episode.AvailableEpisodes.Sub; i++)
 {
@@ -43,12 +52,65 @@ for (var i = 1; i <= episode.AvailableEpisodes.Sub; i++)
 }
 var episodeNumber = AnsiConsole.Prompt(selectEpisodePrompt);
 var sources = await allAnimeClient.GetEpisodeSources(episode.Id, episodeNumber);
-var rightSource = sources.Data.Episode.SourceUrls.First(s => s.SourceName == "S-mp4");
-var urls = await allAnimeClient.GetLinksFromSourceUrl(rightSource.SourceUrl);
 
-if (urls != null)
+var validSourceNames = new[]
 {
-    var url = urls.Links.First();
+    "Default",
+    "Sak",
+    "Kir",
+    "S-mp4",
+    "Luf-mp4"
+};
+var validSources = sources.Data.Episode.SourceUrls
+    .Where(source => validSourceNames.Contains(source.SourceName))
+    .ToList();
+
+var links = new List<LinksFromSourceUrlResponseModel.LinkType>();
+foreach (var source in validSources)
+{
+    try
+    {
+        var urls = await allAnimeClient.GetLinksFromSourceUrl(source.SourceUrl);
+        links.AddRange(urls.Links.Where(l => l.Link != null).ToList());
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+}
+
+var GetResolutionPriority = (string type) =>
+{
+    return type switch
+    {
+        "Mp4" => 1,
+        "hls" => 2,
+        "hls P" => 3,
+        "Alt" => 4,
+        _ => 5
+    };
+};
+links.Sort((a, b) => GetResolutionPriority(a.ResolutionStr).CompareTo(GetResolutionPriority(b.ResolutionStr)));
+
+if (links.Count > 0)
+{
+    var url = links.First();
+
+    if (url.Link != null && url.Link.Contains("repackager.wixmp.com"))
+    {
+        var pattern = @"([^/]*)/mp4";
+        var match = Regex.Match(url.Link, pattern);
+        var resolutions = Array.Empty<string>();
+        var group = string.Empty;
+        if (match.Success)
+        { 
+            group = match.Groups[0].Value.Replace("/mp4", "");
+            resolutions = group.Split(",", StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        url.Link = url.Link.Replace("repackager.wixmp.com/", "").Replace(group, resolutions.First());
+        url.Link = url.Link.Remove(url.Link.IndexOf(".urlset", StringComparison.Ordinal));
+    }
 
     var process = new Process()
     {
